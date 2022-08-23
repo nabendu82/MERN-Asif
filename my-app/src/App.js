d1637059e9b642e57dba2b920f9180eb88ceffb6
nabendu82 <nabendu.biswas@gmail.com>
2022-08-23 07:15:06 +0530

    React Day 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-// import { Greet, ReactGreet } from './components/Greet';
-import Greeting from './components/Greeting';
-import Welcome from './components/Welcome';
+import UserGreeting from './components/UserGreeting';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet /> */}
-      <Greeting name="JavaScript">Language of the web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift">Language for iOS apps</Greeting>
-      <hr />
-      <Welcome name="JavaScript">Language of the web</Welcome>
-      <Welcome name="React Native" />
-      <Welcome name="Java"><button>Click for Java</button></Welcome>
+      <UserGreeting />
     </div>
   );
 }
