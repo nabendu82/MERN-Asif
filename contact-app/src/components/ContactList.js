db1517f8079ab9f0e7156fc438b18526434b7257
nabendu82 <nabendu.biswas@gmail.com>
2022-10-18 07:02:22 +0530

    Router -1

---
+++ contact-app/src/components/ContactList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactCard from './ContactCard'
+
+const ContactList = (props) => {
+    return (
+        <div className='ui celled list'>
+            {props.contacts.map(contact => <ContactCard contact={contact} key={contact.id} />)}
+        </div>
+    )
+}
+
+export default ContactList
