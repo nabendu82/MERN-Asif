89f81ea1503bef548fddb48e6739ef5c24613421
nabendu82 <nabendu.biswas@gmail.com>
2022-11-03 06:57:34 +0530

    Firebase done

---
+++ tinder-clone-asif/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmDH8nN4V2e-sMxmnhqXPjFvz5fasdXFo",
+    authDomain: "tinder-clone-asif.firebaseapp.com",
+    projectId: "tinder-clone-asif",
+    storageBucket: "tinder-clone-asif.appspot.com",
+    messagingSenderId: "207293411524",
+    appId: "1:207293411524:web:8073ed04f7c5550ab11991"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+export default db
---
+++ tinder-clone-mern/client/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmDH8nN4V2e-sMxmnhqXPjFvz5fasdXFo",
+    authDomain: "tinder-clone-asif.firebaseapp.com",
+    projectId: "tinder-clone-asif",
+    storageBucket: "tinder-clone-asif.appspot.com",
+    messagingSenderId: "207293411524",
+    appId: "1:207293411524:web:8073ed04f7c5550ab11991"
+};
